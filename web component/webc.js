f8693ec12132f9b6609f6a5985ec3d40bfda4a5e
eduardogz94 <ezangarini@hotmail.com>
2017-12-01 12:58:00 -0400

    last webc push

--- web component/webc.js
+++ web component/webc.js
@@ -3,4 +3,27 @@
 	nuevo método llamado clear () que permita borrar el contenido del input, luego 
 	construya un programa que utilice el componente.
 */
+	//creando el input element
+	var inputProto = Object.create(HTMLInputElement.prototype);
 
+	//añadiendo propiedades al prototype
+	inputProto.onClear = function(){
+		this.value = "";
+	}
+
+	inputProto.setDimensions = function(){
+		this.style.top = x + "px";
+		this.style.bottom = y + "px";
+	}
+	//registrando componente con su prototype
+	var inputComp = document.registerElement("eg-input",
+		{
+		prototype: inputProto,
+		extends:'input'
+		}
+	);
+
+	//instanciando componente
+	var comp = new inputComp();
+	//registando al body el componente
+	document.body.appendChild(comp);
