9d496181fd023543c7af56305aee368e741544cc
eduardogz94 <ezangarini@hotmail.com>
2017-12-04 00:09:04 -0400

    Details push

--- juego canvas/juego.js
+++ juego canvas/juego.js
@@ -1,77 +1,59 @@
 /* 
-	Haga un programa en javascript que permita mover un rectángulo 
-	construido con canvas, utilizando las flechas: arriba, abajo, 
-	derecha e izquierda. (5pts.)
-*/
-var boton = document.getElementById("boton");
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var ancho = canvas.width;
-var alto = canvas.height;
-var x = canvas.width/2;
-var y = canvas.height - 40;
-var pX = (canvas.width - 15)/2;
+	 Haga un programa en javascript que permita mover un rectángulo 
+	 construido con canvas, utilizando las flechas: arriba, abajo, 
+	 derecha e izquierda. (5pts.)
+      */
+
+//variables globales
+ var boton = document.getElementById("boton");
+ var canvas = document.getElementById("canvas");
+ var ctx = canvas.getContext("2d");
+ var ancho = canvas.width;
+ var alto = canvas.height;
+ var x = canvas.width/2;
+ var y = canvas.height/2;
+ var velocidad = 10;
+ var direccion = (canvas.width- 15)/2;
 	
-	class Rectangle {
-		contructor(x,y){
-			this.x = x;
-			this.y = y;
-			this.w = 1;
-			this.h = 1;
-			this.dir = 0;
-		}
-
-		mover(){
-			this.x = pX;
-		}
-
-		dibujar(){
-			ctx.fillRect(this.x, this.y, this.w, this.h);
-		}
-	}
-
-
-//instanciando nuestro rectangulo
-var test = new Rectangle(x,y);
-
-//funciones de animacion
-	function dibujarRect(){
-		ctx.beginPath()
-		ctx.rect(pX, this.y , 15, 15);
-		ctx.fillStyle = "black";
-		ctx.fill();
-		ctx.closePath();
-	}
 	
-	function dibujar(){
-		ctx.clearRect(0,0,ancho,alto);
-		dibujarRect();
-	}
+ //funciones de animacion
+	 function dibujarRect(){
+		 ctx.rect(direccion, this.y - 15 , 15, 15);
+		 ctx.fillStyle = "black";
+		 ctx.fill();
+	 }
+	
+	 function dibujar(){
+	 	 ctx.clearRect(0,0,ancho,alto);
+		 ctx.beginPath();
+		 dibujarRect();
+		 ctx.closePath();
+	 }
 
-	function frame(){
-		dibujar();
-		requestAnimationFrame(frame);
-	}
+	 function frame(){
+	 	 dibujar();
+		 requestAnimationFrame(frame);
+	 }
 
-	function iniciar(){
-		frame();
-	}
+	 function iniciar(){
+		 frame();
+	 }
 
 
-//funciones de control	
-	function keyDownHandler(event){
-		var tecla = event.keyCode;
-			if(tecla == 39){
-				pX += 15;
+ //funciones de control	
+	 function keyDownHandler(event){
+		 var tecla = event.keyCode;
+			 if(tecla == 39){
+			 	 direccion += velocidad;		
+			 }
+		
+			 else if(tecla == 37){
+			 	 direccion -= velocidad;
 			}
-			else if(tecla == 37){
-				pX -= 15;
-			}
-	}
 
-	function keyUpHandler(event){
-		var tecla = event.keyCode;
-			if(tecla == 39 || tecla == 37){
-				pX = 0;
-			}
-	}
+			else if(tecla == 40){
+				direccion += velodad;
+			}	 
+		}
+
+	 
--- web component/webc.js
+++ web component/webc.js
@@ -1,29 +1,41 @@
 /*
-	Construya un WEBComponent que extienda las propiedades del elemento input y le incluya un 
-	nuevo método llamado clear () que permita borrar el contenido del input, luego 
-	construya un programa que utilice el componente.
-*/
-	//creando el input element
-	var inputProto = Object.create(HTMLInputElement.prototype);
+	 Construya un WEBComponent que extienda las propiedades del elemento input y le incluya un 
+	 nuevo metodo llamado clear () que permita borrar el contenido del input, luego 
+	 construya un programa que utilice el componente.
+     */
+     
+	 //creando el input prototype object
+	 var inputProto = Object.create(HTMLInputElement.prototype);
 
-	//añadiendo propiedades al prototype
-	inputProto.onClear = function(){
-		this.value = "";
-	}
+	 //añadiendo propiedades al prototype
+	 inputProto.onClear = function(){
+		 this.value = "";
+	 }
 
-	inputProto.setDimensions = function(){
-		this.style.top = x + "px";
-		this.style.bottom = y + "px";
-	}
-	//registrando componente con su prototype
-	var inputComp = document.registerElement("eg-input",
-		{
-		prototype: inputProto,
-		extends:'input'
-		}
-	);
+	 inputProto.setDimensions = function(width,height){
+		 this.style.width = width + "px";
+		 this.style.height = height + "px";
+	 }
 
-	//instanciando componente
-	var comp = new inputComp();
-	//registando al body el componente
-	document.body.appendChild(comp);
+	 //registrando componente con su prototype
+	 var inputComp = document.registerElement("eg-input",
+		 {
+		 prototype: inputProto,
+		 extends:'input'
+		 }
+	 );
+
+	 //instanciando componente
+	 var comp = new inputComp();
+	
+	 function test1(){
+		 comp.onClear();
+	 }
+
+	 function test2(){
+		 comp.setDimensions(100,200);
+	 }
+
+
+	 //registando al body el componente
+	 document.body.appendChild(comp);
