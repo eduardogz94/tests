baeee83703da86b6d661e4baf4bf79d84d2975c7
eduardogz94 <ezangarini@hotmail.com>
2017-12-14 01:58:11 -0400

    Stylesheet

--- canvas mouse/canvas.js
+++ canvas mouse/canvas.js
@@ -6,15 +6,9 @@
  var Y = (canvas.height - 15)/2;
 
 
- class Rectangulo {
- 	 constructor(){
- 	  	 this.x = X;
- 	  	 this.y = Y;
- 	  	 this.w = 15;
- 	  	 this.h = 15;
- 	 }
- 	 	 
+ class Rectangulo {	 	 
  	 dibujar(){
+ 	 	 ctx.clearRect(0,0, canvas.width,canvas.height);
  	  	 ctx.rect(X, Y , 15, 15);
 	 	 ctx.fillStyle = "black";
 	 	 ctx.fill();
@@ -24,16 +18,14 @@
  var rect = new Rectangulo();
 
  function actualizar(){
- 	 ctx.fillStyle = "#f0f0f0";
-	 ctx.fillRect(0,0, canvas.width, canvas.height);
 	 rect.dibujar();
  }
 	
  function oMousePos(canvas, evt) {
- 	 var rect = canvas.getBoundingClientRect();
+ 	 var client = canvas.getBoundingClientRect();
   	 return { // devuelve un objeto
-          x: Math.round(evt.clientX - rect.left),
-          y: Math.round(evt.clientY - rect.top)
+          x: Math.round(evt.clientX - client.left),
+          y: Math.round(evt.clientY - client.top)
      };
  }
 
@@ -41,17 +33,18 @@
   	 var mousePos = oMousePos(canvas, evt);
      if (ctx.isPointInPath(mousePos.x, mousePos.y)) {
          onMove = true;
-         actualizar();
          rect.x = X - mousePos.x;
          rect.y = Y - mousePos.y;
+     	 
+         actualizar();
      }
  }, false);
 
  canvas.addEventListener("mousemove", function(evt) {
   	 var mousePos = oMousePos(canvas, evt);
      if (onMove) {
-     	 ctx.clearRect(0,0,canvas.width, canvas.height);
-         X = mousePos.x + rect.x, Y = mousePos.y + rect.y
+         X = mousePos.x + rect.x, 
+         Y = mousePos.y + rect.y
          actualizar();
      }
  }, false);
