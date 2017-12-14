f5aeb4cf0e91adb5c5874a0d2042e219027915ba
eduardogz94 <ezangarini@hotmail.com>
2017-12-13 23:26:07 -0400

    Tests

--- canvas mouse/canvas.js
+++ canvas mouse/canvas.js
@@ -1,45 +1,61 @@
-var button = document.getElementById("button");
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var onMove = false;
-var moves = 2;
-var X = (canvas.width - 15)/2;
-var Y = (canvas.height - 15)/2;
-var rect = new Object();
+ var button = document.getElementById("button");
+ var canvas = document.getElementById("canvas");
+ var ctx = canvas.getContext("2d");
+ var onMove = false;
+ var X = (canvas.width - 15)/2;
+ var Y = (canvas.height - 15)/2;
 
- //funcion objeto
 
- 		function dibujarRect(){
-		 ctx.rect(X, Y , 15, 15);
-		 ctx.fillStyle = "black";
-		 ctx.fill();
-	 	}
+ class Rectangulo {
+ 	 constructor(){
+ 	  	 this.x = X;
+ 	  	 this.y = Y;
+ 	  	 this.w = 15;
+ 	  	 this.h = 15;
+ 	 }
+ 	 	 
+ 	 dibujar(){
+ 	  	 ctx.rect(X, Y , 15, 15);
+	 	 ctx.fillStyle = "black";
+	 	 ctx.fill();
+ 	 }
+ }
+
+ var rect = new Rectangulo();
+
+ function actualizar(){
+ 	 ctx.fillStyle = "#f0f0f0";
+	 ctx.fillRect(0,0, canvas.width, canvas.height);
+	 rect.dibujar();
+ }
 	
-		 function oMousePos(canvas, evt) {
-      	 	 var rect = canvas.getBoundingClientRect();
-      	 	 return { // devuelve un objeto
-         	 x: Math.round(evt.clientX - rect.left),
-         	 y: Math.round(evt.clientY - rect.top)
-         	 };
-         }
+ function oMousePos(canvas, evt) {
+ 	 var rect = canvas.getBoundingClientRect();
+  	 return { // devuelve un objeto
+          x: Math.round(evt.clientX - rect.left),
+          y: Math.round(evt.clientY - rect.top)
+     };
+ }
 
-	 	 canvas.addEventListener("mousedown", function(evt) {   
-       	 	 var mousePos = oMousePos(canvas, evt);
-         	 dibujarRect();
-         	 if (ctx.isPointInPath(mousePos.x, mousePos.y)) {
-         	 onMove = true;
-         	 rect.x = X - mousePos.x;
-         	 rect.y = Y - mousePos.y;
-       		 }
-    	 }, false);
+ canvas.addEventListener("mousedown", function(evt) {   
+  	 var mousePos = oMousePos(canvas, evt);
+     if (ctx.isPointInPath(mousePos.x, mousePos.y)) {
+         onMove = true;
+         actualizar();
+         rect.x = X - mousePos.x;
+         rect.y = Y - mousePos.y;
+     }
+ }, false);
 
-     	 canvas.addEventListener("mousemove", function(evt) {
-       		 var mousePos = oMousePos(canvas, evt);
-         	 if (onMove) {
-         	 ctx.clearRect(0, 0, canvas.width, canvas.height);
-         	 X = mousePos.x + rect.x, Y = mousePos.y + rect.y
-         	 dibujarRect();
-         	 }
-     	 }, false);
+ canvas.addEventListener("mousemove", function(evt) {
+  	 var mousePos = oMousePos(canvas, evt);
+     if (onMove) {
+     	 ctx.clearRect(0,0,canvas.width, canvas.height);
+         X = mousePos.x + rect.x, Y = mousePos.y + rect.y
+         actualizar();
+     }
+ }, false);
 
-
+ canvas.addEventListener("mouseup", function(evt) {
+  	 onMove = false;
+ }, false);
